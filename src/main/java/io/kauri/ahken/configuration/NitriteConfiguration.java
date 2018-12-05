620c9e9855472eb88f56ca895b9097e1d74b3b04
Craig <craigwilliams84@googlemail.com>
2018-12-05 00:33:05 +0000

    Create db path if it doesn't exist

--- src/main/java/io/kauri/ahken/configuration/NitriteConfiguration.java
+++ src/main/java/io/kauri/ahken/configuration/NitriteConfiguration.java
@@ -1,6 +1,10 @@
 package io.kauri.ahken.configuration;
 
 import io.kauri.ahken.AhkenSettings;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.dizitart.no2.Nitrite;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +12,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class NitriteConfiguration {
 
+    private static final String DB_FILE_NAME = "db.dat";
+
     @Bean
-    public Nitrite nitrite(AhkenSettings settings) {
+    public Nitrite nitrite(AhkenSettings settings) throws IOException {
+        final Path dbLocation = Paths.get(settings.getDbLocation());
+
+        if (!Files.exists(dbLocation)) {
+            Files.createDirectories(dbLocation);
+        }
+
         return Nitrite.builder()
                 .compressed()
-                .filePath(settings.getDbLocation())
+                .filePath(settings.getDbLocation() + "/" + DB_FILE_NAME)
                 .openOrCreate();
     }
 }
